929418561c104ea83d6191148efa999b07dc3cd0
mingyupar <119020575+mingyupar@users.noreply.github.com>
2022-11-25 20:36:20 +0900

    Update Bus.java

--- sparta_java/Transportation/Bus.java
+++ sparta_java/Transportation/Bus.java
@@ -8,3 +8,29 @@ public class Bus extends PublicTransportation {
         System.out.println(busNum + "번 버스가 생성되었습니다.");
     }
 }
+
+public class Student {
+	//1번
+	String studentName;
+	int grade;
+	int money;
+	//2번
+	public Student(String studentName, int money) {
+		
+		this.studentName = studentName;
+		this.money = money;
+	}
+	//3번
+	public void takeBus(Bus bus) {
+		bus.take(1000);
+		money -= 1000;
+	}
+	//4번
+	public void takeSubway(Subway subway) {
+		subway.take(1200);
+		money -= 1200;
+	}
+	//5번
+	public void showInfo() {
+		System.out.println(studentName + "의 남은 돈은 "+money+"입니다.");
+	}
